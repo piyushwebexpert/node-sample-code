e181da751310ca98b88b5949c05bfba52b1af04f
Piyush <piyush.webexpert@gmail.com>
2024-10-14 21:03:51 +0530

    changes in seed file

--- src/seed.ts
+++ src/seed.ts
@@ -13,6 +13,7 @@ conn.once('open', async () => {
   console.log('Connected successfully to MongoDB');
   console.log('Connected successfully to MongoDB');
   console.log('Connected successfully to MongoDB');  
+  console.log('Connected successfully to MongoDB');  
     
 
   // Define the Invoice Schema
